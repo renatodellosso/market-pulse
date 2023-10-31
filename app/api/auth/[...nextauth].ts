4940a830469938dbe58dc4124f64ea7696dee857
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2023-10-31 19:59:18 -0400

    Initial commit

---
+++ app/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        })
+    ]
+}
+
+export default NextAuth(authOptions);

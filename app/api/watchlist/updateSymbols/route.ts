8afcbe222d3ea80871aff4d132586acc23749e50
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2023-12-11 19:05:40 -0500

    Refactor watchlist symbol update route

--- app/api/watchlist/updateSymbols/route.ts
+++ app/api/watchlist/updateSymbols/route.ts
@@ -8,28 +8,36 @@ import { getWatchlist, updateName, updateSymbols } from "@/lib/db/watchlists";
 import { ObjectId } from "mongodb";
 
 export async function GET(req: NextRequest) {
-  const session = await getServerSession(authOptions);
+  try {
+    const session = await getServerSession(authOptions);
 
-  if (!session)
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    if (!session)
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
-  // Read query params
-  const params = new URL(req.url!).searchParams;
-  let id: ObjectId | string = params.get("id")!;
-  const symbols = JSON.parse(params.get("symbols")!);
+    // Read query params
+    const params = new URL(req.url!).searchParams;
+    let id: ObjectId | string = params.get("id")!;
+    const symbols = JSON.parse(params.get("symbols")!);
 
-  console.log("Updating watchlist symbols: " + symbols);
+    console.log("Updating watchlist symbols: " + symbols);
 
-  const watchlist = await getWatchlist(id);
+    const watchlist = await getWatchlist(id);
 
-  // Make sure to verify watchlist exists and user has permissions!
-  if (!watchlist)
-    return NextResponse.json({ error: "Watchlist not found" }, { status: 404 });
-  if (watchlist.ownerEmail != session.user.email)
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    // Make sure to verify watchlist exists and user has permissions!
+    if (!watchlist)
+      return NextResponse.json(
+        { error: "Watchlist not found" },
+        { status: 404 }
+      );
+    if (watchlist.ownerEmail != session.user.email)
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
-  id = new ObjectId(id!);
-  updateSymbols(id, symbols!);
+    id = new ObjectId(id!);
+    updateSymbols(id, symbols!);
 
-  return NextResponse.json({ id: id, symbols: symbols });
+    return NextResponse.json({ id: id, symbols: symbols });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ error: e }, { status: 500 });
+  }
 }

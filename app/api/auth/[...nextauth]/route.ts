0f99eb25bb30afd5eae1fc0a2f83b997fe106a39
Renato Dell'Osso <54045197+renatodellosso@users.noreply.github.com>
2023-12-23 12:34:48 -0500

    Refactor authentication and remove unused code

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -17,7 +17,6 @@ export const authOptions: AuthOptions = {
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-<<<<<<< HEAD
       if (
         user.reports === undefined ||
         user.watchlists === undefined ||
@@ -28,12 +27,6 @@ export const authOptions: AuthOptions = {
         console.log("Initializing user...");
         initUser(user);
       }
-=======
-      // if(user.reports === undefined) {
-      //     console.log("Initializing user...");
-      //     initUser(user);
-      // }
->>>>>>> 3ff5ce9924251a2a24fc7dafae90ecf3c3812bbe
 
       return true;
     },
